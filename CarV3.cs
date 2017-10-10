91829922739148101d68900b5aa173625caf18db
Alan Falcon <falconswoosh@gmail.com>
2017-10-10 06:32:12 -0700

    add CarV3

---
+++ CarV3.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+class Car
+{
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+
+  public Car(string carMakeModel, int carPrice, int carMiles)
+  {
+    MakeModel = carMakeModel;
+    Price = carPrice;
+    Miles = carMiles;
+  }
+public class Program
+{
+  public static void Main()
+  {
+    Car porsche = new Car("2014 Porsche 911", 114991, 7864);
+    Car ford = new Car("2011 Ford F450", 55995, 14241);
+    Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+
+    List<Car> Cars = new List<Car>() {porsche, ford, lexus, mercedes };
+
+    foreach (Car automobile in Cars)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
+    Console.WriteLine("Enter maximum price: ");
+    string stringMaxPrice = Console.ReadLine();
+    int maxPrice = int.Parse(stringMaxPrice);
+
+    List<Car> CarsMatchingSearch = new List<Car>();
+
+    foreach (Car automobile in Cars)
+    {
+      if (automobile.Price < maxPrice)
+      {
+        CarsMatchingSearch.Add(automobile);
+      }
+    }
+    foreach (Car automobile in CarsMatchingSearch)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
+  }
+}
+}
